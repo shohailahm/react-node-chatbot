a577f71d427594197c983c63b8231731bc2179a1
suhail <suhail@onebitconsult.com>
2019-08-22 12:28:08 +0530

    adding front-end

---
+++ frontend/src/ChatInput.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+
+class ChatInput extends Component {
+ 
+  state = {
+    message: '',
+  }
+
+  render() {
+    return (
+      <form
+        action="."
+        onSubmit={e => {
+          e.preventDefault()
+          this.props.onSubmitMessage(this.state.message)
+          this.setState({ message: '' })
+        }}
+      >
+        <input
+          type="text"
+          placeholder={'Enter message...'}
+          value={this.state.message}
+          onChange={e => this.setState({ message: e.target.value })}
+        />
+        <input type="submit" value={'Send'} />
+      </form>
+    )
+  }
+}
+
+export default ChatInput
