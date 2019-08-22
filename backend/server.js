092d751fe3857997eea65672c10cb7d61a63fcab
suhail <suhail@onebitconsult.com>
2019-08-22 12:18:51 +0530

    frontend

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,11 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 3030 });
 
 wss.on('connection', function connection(ws) {
+  console.log("connected");
   ws.on('message', function incoming(data) {
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(data);
+          client.send(data);
       }
     });
   });
